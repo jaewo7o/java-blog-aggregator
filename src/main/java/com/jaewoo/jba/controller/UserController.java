2a81390fed31c246854700295efd2c980a83b32b
jaewo7o <jaewo7o@naver.com>
2015-02-14 11:01:32 +0900

    Hibernate validation

--- src/main/java/com/jaewoo/jba/controller/UserController.java
+++ src/main/java/com/jaewoo/jba/controller/UserController.java
@@ -2,9 +2,12 @@ package com.jaewoo.jba.controller;
 
 import java.security.Principal;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,7 +55,11 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String doRegister(@ModelAttribute("user") User user) {
+	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result) {
+		if (result.hasErrors()) {
+			return "user-register";
+		}
+		
 		userService.save(user);
 		return "redirect:/register.html?success=true";
 	}
@@ -65,7 +72,10 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/account", method=RequestMethod.POST)
-	public String addBlog(@ModelAttribute("blog") Blog blog, Principal principal) {
+	public String addBlog(Model model, @Valid @ModelAttribute("blog") Blog blog, BindingResult result, Principal principal) {
+		if (result.hasErrors()) {
+			return account(model, principal);
+		}
 		String name = principal.getName();
 		blogService.save(blog, name);
 		return "redirect:/account.html?success=true";
